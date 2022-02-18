a86f05700b17b442b6502e48e532b7ffc235c832
Dima Velychko <surokdimkus@gmail.com>
2022-02-18 14:50:30 +0200

    Fix header

--- src/components/PageHeader.tsx
+++ src/components/PageHeader.tsx
@@ -3,9 +3,9 @@ import styled from 'styled-components';
 const PageHeader = styled.div`
   display: flex;
   justify-content: space-between;
+  align-items: center;
   background: ${(props) => props.theme.primary};
-  padding: 50px 0 25px 0;
-  width: 100%;
+  height: 100px;
 `
 
 export default PageHeader;
